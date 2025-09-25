7e61aedf54d58296809999337eae3babcac7cfb2
Adryel Felipe <adryxd003@gmail.com>
2025-09-25 11:03:49 -0300

    ✨ Fit: Altera a interface IProduto
    Adiciona os métodos abstratos getNome e getPreco

--- src/main/java/Arquitetura/Models/Eletronico.java
+++ src/main/java/Arquitetura/Models/Eletronico.java
@@ -1,4 +1,4 @@
 package Arquitetura.Models;
 
-public class Eletronico {
+public class Eletronico implements{
 }
--- src/main/java/Arquitetura/Models/IProduto.java
+++ src/main/java/Arquitetura/Models/IProduto.java
@@ -1,4 +1,9 @@
 package Arquitetura.Models;
 
 public interface IProduto {
+
+    // MÉTODOS ABASTRATOS
+    String getNome();
+
+    String getPreco();
 }
