36897f293b350d307d997adf07d34e21c5e41f71
Adryel Felipe <adryxd003@gmail.com>
2025-09-25 11:53:03 -0300

    ✨ Fit: Altera a ProdutoDao
    Cria a lista de produtos e adiciona métodos para a manipular

--- src/main/java/Arquitetura/Daos/ProdutoDao.java
+++ src/main/java/Arquitetura/Daos/ProdutoDao.java
@@ -1,4 +1,29 @@
 package Arquitetura.Daos;
 
+import Arquitetura.Models.IProduto;
+
+import javax.swing.plaf.OptionPaneUI;
+import java.util.ArrayList;
+
 public class ProdutoDao {
+
+    // Atributos
+    private static final ArrayList<IProduto> produtos = new ArrayList<>();
+
+    // Construtor
+    public ProdutoDao() {
+
+    }
+
+    // Métodos
+
+    // ---------- ADICIONA PRODUTOS NA LISTA DE PRODUTOS ---------- //
+    public void adicionarProduto(IProduto produto) {
+        produtos.add(produto);
+    }
+
+    // ---------- LISTA OS PRODUTOS DA ARRAYLIST DE PRODUTOS ---------- //
+    public ArrayList<IProduto> listarProdutos() {
+        return produtos;
+    }
 }
